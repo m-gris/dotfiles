dc749ce382ffc124045a6d67568873fb95114126
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2023-09-07 16:15:51 +0200

    comment hammerspoon vim mode cause: requires a git repo inside the hammerspoon conf dir which causes some issues with the .dotfiles repo itself need further investigations...

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -24,48 +24,48 @@ hs.hotkey.bind({"alt", "ctrl"}, "M", open_app("Microsoft Teams"))
 hs.hotkey.bind({"alt", "ctrl"}, "T", open_app("Kitty"))
 
 
---------------------------------
--- START VIM CONFIG
---------------------------------
-local VimMode = hs.loadSpoon("VimMode")
-local vim = VimMode:new()
+----------------------------------
+---- START VIM CONFIG
+----------------------------------
+--local VimMode = hs.loadSpoon("VimMode")
+--local vim = VimMode:new()
 
--- Configure apps you do *not* want Vim mode enabled in
--- For example, you don't want this plugin overriding your control of Terminal
--- vim
-vim
-  :disableForApp('Code')
-  :disableForApp('zoom.us')
-  :disableForApp('iTerm')
-  :disableForApp('iTerm2')
-  :disableForApp('Terminal')
-  :disableForApp('Code')
+---- Configure apps you do *not* want Vim mode enabled in
+---- For example, you don't want this plugin overriding your control of Terminal
+---- vim
+--vim
+--  :disableForApp('Code')
+--  :disableForApp('zoom.us')
+--  :disableForApp('iTerm')
+--  :disableForApp('iTerm2')
+--  :disableForApp('Terminal')
+--  :disableForApp('Code')
 
--- If you want the screen to dim (a la Flux) when you enter normal mode
--- flip this to true.
-vim:shouldDimScreenInNormalMode(false)
+---- If you want the screen to dim (a la Flux) when you enter normal mode
+---- flip this to true.
+--vim:shouldDimScreenInNormalMode(false)
 
--- If you want to show an on-screen alert when you enter normal mode, set
--- this to true
-vim:shouldShowAlertInNormalMode(true)
+---- If you want to show an on-screen alert when you enter normal mode, set
+---- this to true
+--vim:shouldShowAlertInNormalMode(true)
 
--- You can configure your on-screen alert font
-vim:setAlertFont("Courier New")
+---- You can configure your on-screen alert font
+--vim:setAlertFont("Courier New")
 
--- Enter normal mode by typing a key sequence
--- vim:enterWithSequence('asdf', 250)
+---- Enter normal mode by typing a key sequence
+---- vim:enterWithSequence('asdf', 250)
 
--- if you want to bind a single key to entering vim, remove the
--- :enterWithSequence('jk') line above and uncomment the bindHotKeys line
--- below:
---
--- To customize the hot key you want, see the mods and key parameters at:
---   https://www.hammerspoon.org/docs/hs.hotkey.html#bind
---
-vim:bindHotKeys({ enter = { {'ctrl'}, ';' } })
-vim:enableBetaFeature('block_cursor_overlay')
+---- if you want to bind a single key to entering vim, remove the
+---- :enterWithSequence('jk') line above and uncomment the bindHotKeys line
+---- below:
+----
+---- To customize the hot key you want, see the mods and key parameters at:
+----   https://www.hammerspoon.org/docs/hs.hotkey.html#bind
+----
+--vim:bindHotKeys({ enter = { {'ctrl'}, ';' } })
+--vim:enableBetaFeature('block_cursor_overlay')
 
 
---------------------------------
--- END VIM CONFIG
---------------------------------
+----------------------------------
+---- END VIM CONFIG
+----------------------------------
