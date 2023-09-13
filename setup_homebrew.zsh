25e721a08f8fb79d07ea09ab56ad57de04eaaacb
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2023-09-14 00:19:03 +0200

    use 'brew bundle' cmd to install everything in Brewfile

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,8 +4,5 @@ echo "\n <<< Starting HOMEBREW setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install httpie
-brew install bat 
-
-# --no-quarantine: arg that will bypass / turn-off macOs "gatekeeper" ("Are you sure you want to install X...")
-brew install --no-quarantine visual-studio-code
+# install all packages / apps in the Brewfile
+brew bundle --verbose
