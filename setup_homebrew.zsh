03fb1f65de303b7166a170dc3d91d494405fa9d6
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2023-09-16 09:40:06 +0200

    use exist func to avoid re-installing brew

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -2,7 +2,12 @@
 
 echo "\n <<< Starting HOMEBREW setup >>>\n"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if exists brew; then
+    echo "brew exists ! skipping install"
+else
+    echo "brew does not exist ! continuing with install"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi 
 
 # NB: in zshrc HOMEBREW_CASK_OPTS='--no-quarantine' to bypass macOs gatekeeper
 #
