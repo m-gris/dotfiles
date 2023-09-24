f52bd8cd40fa6f5d57e86a8132c936f753ed776a
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2023-09-24 20:04:58 +0200

    add gitisigns config in init.lua

--- config/PrimeVim/init.lua
+++ config/PrimeVim/init.lua
@@ -4,6 +4,7 @@ require("theprimeagen.packer")
 require("telescope").load_extension("git_worktree")
 require("telescope").load_extension("file_browser")
 
+require('lualine').setup()
 
 
 vim.cmd([[colorscheme catppuccin]])
