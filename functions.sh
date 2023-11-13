92e6dbb04f129d4edc02313904ccbeffe6309739
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2023-11-13 10:06:01 +0100

    clojure repl functions

--- functions.sh
+++ functions.sh
@@ -47,3 +47,15 @@ function auto_activate_venv() {
 }
 
 
+# CLOJURE REPL: syntax highlighting, autcompletion etc... 
+# Add these to your .bash_profile / .zshrc / etc.
+
+# Starts a Clojure repl
+function rebel-clj() {
+  clojure -Sdeps "{:deps {com.bhauman/rebel-readline {:mvn/version \"0.1.4\"} $@}}" -m rebel-readline.main
+}
+
+# Starts a browser REPL
+function rebel-cljs() {
+  clojure -Sdeps "{:deps {com.bhauman/figwheel-main {:mvn/version \"0.1.7\"} com.bhauman/rebel-readline-cljs {:mvn/version \"0.1.4\"} $@}}" -m figwheel.main
+}
