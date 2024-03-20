8682667a7371c39dcc0c545abd310f202cdbd28a
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2024-03-20 18:54:58 +0800

    nvim: comment section of init.lua
    it bound C-h to something other than backspace in insert mode

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -712,11 +712,11 @@ require('lazy').setup({
               luasnip.expand_or_jump()
             end
           end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
+          -- ['<C-h>'] = cmp.mapping(function()
+          --   if luasnip.locally_jumpable(-1) then
+          --     luasnip.jump(-1)
+          --   end
+          -- end, { 'i', 's' }),
 
           -- For more advanced luasnip keymaps (e.g. selecting choice nodes, expansion) see:
           --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
