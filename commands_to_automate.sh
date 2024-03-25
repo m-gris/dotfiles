521160298cc1ba401418ca8063e1ec48cc7229a4
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2024-03-25 12:20:08 +0800

    add commands_to_automate

---
+++ commands_to_automate.sh
@@ -0,0 +1,3 @@
+# Disable popup showing accented characters when holding down a key
+# source: https://apple.stackexchange.com/questions/332769/macos-disable-popup-showing-accented-characters-when-holding-down-a-key:wq
+defaults write -g ApplePressAndHoldEnabled -bool false
